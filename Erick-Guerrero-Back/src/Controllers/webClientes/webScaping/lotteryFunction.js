772d8436d20ed586334ea91605c0830acae23205
Gaston Schmitz <108362008+Schmitz0@users.noreply.github.com>
2024-03-13 14:44:25 -0300

    a

--- Erick-Guerrero-Back/src/Controllers/webClientes/webScaping/lotteryFunction.js
+++ Erick-Guerrero-Back/src/Controllers/webClientes/webScaping/lotteryFunction.js
@@ -2,91 +2,188 @@ const puppeteer = require("puppeteer");
 const { Numbers, Tickets, User, TicketNumber } = require("../../../db");
 const { Op } = require("sequelize");
 
+// async function lotedom1355() {
+//   let browser;
+//   let page;
+
+//   try {
+//     const today = new Date();
+//     today.setHours(today.getHours() - 3);
+
+//     const startOfDay = new Date(today);
+// startOfDay.setHours(0, 0, 0, 0); // Establecer la hora a las 00:00:00
+
+// const endOfDay = new Date(today);
+// endOfDay.setHours(23, 59, 59, 999); 
+
+//     const numberAlready = await Numbers.findOne({
+//       where: {
+//         nameLottery: "LoteDom",
+//         hr: "13:55",
+//         day:{
+//             [Op.between]: [startOfDay, endOfDay],
+// }
+//       },
+//     });
+
+//     if (numberAlready) return "Ok";
+
+//     browser = await puppeteer.launch({
+//       headless: "new",
+//       args: ["--no-sandbox"],
+//     });
+
+//     page = await browser.newPage();
+
+//     const url = "https://lotedom.com/";
+//     await page.goto(url, { waitUntil: "domcontentloaded" });
+//     await page.waitForSelector(".col-10.div_resultados", { timeout: 30000 }); // Espera 60 segundos en lugar de 30.
+
+//     const numbersData = await page.evaluate(() => {
+//       const divResultados = Array.from(
+//         document.querySelectorAll(".col-10.div_resultados")
+//       );
+
+//       const data = divResultados.map((div) => {
+//         const numeros = Array.from(
+//           div.querySelectorAll(".num_bola_blanca")
+//         ).map((span) => span.textContent.trim());
+//         return numeros;
+//       });
+
+//       return data;
+//     });
+
+//     if (numbersData.length > 0 && numbersData[0].length === 3) {
+//       const numbers = numbersData[0];
+
+//       // Verificar si el número es "00" y ajustar el valor
+//       const adjustedNumbers = numbers.map((num) =>
+//         num === "00" ? "100" : num
+//       );
+
+//       await Numbers.create({
+//         number1: adjustedNumbers[0],
+//         number2: adjustedNumbers[1],
+//         number3: adjustedNumbers[2],
+//         page: "https://lotedom.com/",
+//         nameLottery: "LoteDom",
+//         hr: "13:55",
+//         imageUrl:
+//           "https://enloteria.com/assets/lotedom-9aae43273ce4d8d4d5429f6f57f2fadc54012eb96fc80d6f59cfb9b72576b7e9.svg",
+//         day: today,
+//       });
+//     } else {
+//       console.log("No se encontraron números válidos en la página.");
+//     }
+//   } catch (error) {
+//     console.error(
+//       "Se produjo un error al ejecutar la función lotedom1355:",
+//       error
+//     );
+//   } finally {
+//     if (page) {
+//       await page.close();
+//     }
+//     if (browser) {
+//       await browser.close();
+//     }
+//   }
+// }
+
 async function lotedom1355() {
+  const maxRetries = 3;
+  let retries = 0;
   let browser;
   let page;
 
-  try {
-    const today = new Date();
-    today.setHours(today.getHours() - 3);
+  while (retries < maxRetries) {
+    try {
+      const today = new Date();
+      today.setHours(today.getHours() - 3);
 
-    const startOfDay = new Date(today);
+      const startOfDay = new Date(today);
 startOfDay.setHours(0, 0, 0, 0); // Establecer la hora a las 00:00:00
 
 const endOfDay = new Date(today);
 endOfDay.setHours(23, 59, 59, 999); 
 
-    const numberAlready = await Numbers.findOne({
-      where: {
-        nameLottery: "LoteDom",
-        hr: "13:55",
-        day:{
-            [Op.between]: [startOfDay, endOfDay],
+const numberAlready = await Numbers.findOne({
+  where: {
+    nameLottery: "LoteDom",
+    hr: "13:55",
+    day:{
+        [Op.between]: [startOfDay, endOfDay],
 }
-      },
-    });
+  },
+});
 
-    if (numberAlready) return "Ok";
+      if (numberAlready) return "Ok";
 
-    browser = await puppeteer.launch({
-      headless: "new",
-      args: ["--no-sandbox"],
-    });
-
-    page = await browser.newPage();
-
-    const url = "https://lotedom.com/";
-    await page.goto(url, { waitUntil: "domcontentloaded" });
-    await page.waitForSelector(".col-10.div_resultados", { timeout: 30000 }); // Espera 60 segundos en lugar de 30.
-
-    const numbersData = await page.evaluate(() => {
-      const divResultados = Array.from(
-        document.querySelectorAll(".col-10.div_resultados")
-      );
-
-      const data = divResultados.map((div) => {
-        const numeros = Array.from(
-          div.querySelectorAll(".num_bola_blanca")
-        ).map((span) => span.textContent.trim());
-        return numeros;
+      browser = await puppeteer.launch({
+        headless: "new",
+        args: ["--no-sandbox"],
       });
 
-      return data;
-    });
+      page = await browser.newPage();
+      const url =
+        "https://www.conectate.com.do/loterias/lotedom";
 
-    if (numbersData.length > 0 && numbersData[0].length === 3) {
-      const numbers = numbersData[0];
+      await page.goto(url, { waitUntil: "domcontentloaded" });
+      await page.waitForSelector(".game-scores", { timeout: 30000 });
 
-      // Verificar si el número es "00" y ajustar el valor
-      const adjustedNumbers = numbers.map((num) =>
-        num === "00" ? "100" : num
-      );
+      const numbersData = await page.evaluate(() => {
+        const container = Array.from(document.querySelectorAll(".game-scores"));
 
-      await Numbers.create({
-        number1: adjustedNumbers[0],
-        number2: adjustedNumbers[1],
-        number3: adjustedNumbers[2],
-        page: "https://lotedom.com/",
-        nameLottery: "LoteDom",
-        hr: "13:55",
-        imageUrl:
-          "https://enloteria.com/assets/lotedom-9aae43273ce4d8d4d5429f6f57f2fadc54012eb96fc80d6f59cfb9b72576b7e9.svg",
-        day: today,
+        const numbers = container.map((element) => {
+          const numberBubbles = Array.from(element.querySelectorAll(".score"));
+          const numbers = numberBubbles.map((bubble) =>
+            bubble.textContent.trim()
+          );
+          return numbers;
+        });
+
+        return numbers;
       });
-    } else {
-      console.log("No se encontraron números válidos en la página.");
-    }
-  } catch (error) {
-    console.error(
-      "Se produjo un error al ejecutar la función lotedom1355:",
-      error
-    );
-  } finally {
-    if (page) {
-      await page.close();
-    }
-    if (browser) {
-      await browser.close();
+
+      if (numbersData.length > 0 && numbersData[0].length === 3) {
+        const numbers = numbersData[0];
+
+        // Verificar si el número es "00" y ajustar el valor
+        const adjustedNumbers = numbers.map((num) =>
+          num === "00" ? "100" : num
+        );
+
+        await Numbers.create({
+          number1: adjustedNumbers[0],
+          number2: adjustedNumbers[1],
+          number3: adjustedNumbers[2],
+          page: "https://lotedom.com/",
+          nameLottery: "LoteDom",
+          hr: "13:55",
+          imageUrl:
+            "https://enloteria.com/assets/lotedom-9aae43273ce4d8d4d5429f6f57f2fadc54012eb96fc80d6f59cfb9b72576b7e9.svg",
+          day: today,
+        });
+
+        break;
+      } else {
+        console.log("No se encontraron números válidos en la página.");
+      }
+    } catch (error) {
+      console.error(
+        "Se produjo un error al ejecutar la función newYorkTarde:",
+        error
+      );
+      retries++;
+    } finally {
+      if (page) {
+        await page.close();
+      }
+      if (browser) {
+        await browser.close();
+      }
+      await new Promise((resolve) => setTimeout(resolve, 5000));
     }
   }
 }
--- Erick-Guerrero-Back/src/Controllers/webClientes/webScaping/prueba.js
+++ Erick-Guerrero-Back/src/Controllers/webClientes/webScaping/prueba.js
@@ -3,13 +3,13 @@ const puppeteer = require("puppeteer");
 // const { Op } = require("sequelize");
 
 
-async function laPrimeraDia() {
+async function lotedom1355() {
   const maxRetries = 3;
-  let retryCount = 0;
-  const today = new Date();
+  let retries = 0;
   let browser;
+  let page;
 
-  while (retryCount < maxRetries) {
+  while (retries < maxRetries) {
     try {
       const today = new Date();
       today.setHours(today.getHours() - 3);
@@ -20,15 +20,15 @@ startOfDay.setHours(0, 0, 0, 0); // Establecer la hora a las 00:00:00
 const endOfDay = new Date(today);
 endOfDay.setHours(23, 59, 59, 999); 
 
-      const numberAlready = await Numbers.findOne({
-        where: {
-          nameLottery: "La Primera Día",
-          hr: "12:00",
-day:{
-            [Op.between]: [startOfDay, endOfDay],
+const numberAlready = await Numbers.findOne({
+  where: {
+    nameLottery: "LoteDom",
+    hr: "13:55",
+    day:{
+        [Op.between]: [startOfDay, endOfDay],
 }
-        },
-      });
+  },
+});
 
       if (numberAlready) return "Ok";
 
@@ -36,14 +36,17 @@ day:{
         headless: "new",
         args: ["--no-sandbox"],
       });
-      const page = await browser.newPage();
-      const url = "https://www.conectate.com.do/loterias/la-primera";
+
+      page = await browser.newPage();
+      const url =
+        "https://www.conectate.com.do/loterias/lotedom";
 
       await page.goto(url, { waitUntil: "domcontentloaded" });
-      await page.waitForSelector(".game-scores", { timeout: 150000 });
+      await page.waitForSelector(".game-scores", { timeout: 30000 });
 
       const numbersData = await page.evaluate(() => {
         const container = Array.from(document.querySelectorAll(".game-scores"));
+
         const numbers = container.map((element) => {
           const numberBubbles = Array.from(element.querySelectorAll(".score"));
           const numbers = numberBubbles.map((bubble) =>
@@ -51,6 +54,7 @@ day:{
           );
           return numbers;
         });
+
         return numbers;
       });
 
@@ -66,31 +70,125 @@ day:{
           number1: adjustedNumbers[0],
           number2: adjustedNumbers[1],
           number3: adjustedNumbers[2],
-          page: "https://www.conectate.com.do/loterias/la-primera",
-          nameLottery: "La Primera Día",
-          hr: "12:00",
+          page: "https://lotedom.com/",
+          nameLottery: "LoteDom",
+          hr: "13:55",
           imageUrl:
-            "https://laprimera.do/wp-content/uploads/2023/07/logo-la-primera.svg",
+            "https://enloteria.com/assets/lotedom-9aae43273ce4d8d4d5429f6f57f2fadc54012eb96fc80d6f59cfb9b72576b7e9.svg",
           day: today,
         });
+
+        break;
       } else {
         console.log("No se encontraron números válidos en la página.");
       }
-
-      await browser.close();
-      return;
     } catch (error) {
-      retryCount++;
-      console.error(`Error en el intento ${retryCount}:`, error);
-      if (retryCount >= maxRetries) {
-        console.error(
-          "Se alcanzó el número máximo de intentos. No se pudo completar la operación."
-        );
-      }
+      console.error(
+        "Se produjo un error al ejecutar la función newYorkTarde:",
+        error
+      );
+      retries++;
     } finally {
+      if (page) {
+        await page.close();
+      }
       if (browser) {
         await browser.close();
       }
+      await new Promise((resolve) => setTimeout(resolve, 5000));
     }
   }
 }
+
+
+async function real1300() {
+  let browser;
+  let page;
+
+  try {
+    const today = new Date();
+    today.setHours(today.getHours() - 3);
+
+    const startOfDay = new Date(today);
+startOfDay.setHours(0, 0, 0, 0); // Establecer la hora a las 00:00:00
+
+const endOfDay = new Date(today);
+endOfDay.setHours(23, 59, 59, 999); 
+
+    const numberAlready = await Numbers.findOne({
+      where: {
+        nameLottery: "Real",
+        hr: "13:00",
+        day:{
+            [Op.between]: [startOfDay, endOfDay],
+}
+      },
+    });
+
+    if (numberAlready) return "Ok";
+
+    browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox"],
+    });
+
+    page = await browser.newPage();
+    const url = "https://www.lotoreal.com.do/";
+
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+    await page.waitForSelector(".result__bowls-container", { timeout: 150000 });
+
+    const numbersData = await page.evaluate(() => {
+      const container = Array.from(
+        document.querySelectorAll(".result__bowls-container")
+      );
+
+      const numbers = container.map((element) => {
+        const numberBubbles = Array.from(element.querySelectorAll(".bolo"));
+        const numbers = numberBubbles.map((bubble) =>
+          bubble.textContent.trim()
+        );
+        return numbers;
+      });
+
+      return numbers;
+    });
+
+    if (numbersData.length > 1 && numbersData[1].length === 3) {
+      const numbers = numbersData[1];
+
+      // Verificar si el número es "00" y ajustar el valor
+      const adjustedNumbers = numbers.map((num) =>
+        num === "00" ? "100" : num
+      );
+
+      await Numbers.create({
+        number1: adjustedNumbers[0],
+        number2: adjustedNumbers[1],
+        number3: adjustedNumbers[2],
+        page: "https://www.lotoreal.com.do/",
+        nameLottery: "Real",
+        hr: "13:00",
+        imageUrl:
+          "https://enloteria.com/assets/real-eeb33736cd36eff0dfb219af8954fe0ab37245bd412801ea864f6a131c3c758c.svg",
+        day: today,
+      });
+    } else {
+      console.log("No se encontraron números válidos en la página.");
+    }
+  } catch (error) {
+    console.error(
+      "Se produjo un error al ejecutar la función real1300:",
+      error
+    );
+  } finally {
+    if (page) {
+      await page.close();
+    }
+    if (browser) {
+      await browser.close();
+    }
+  }
+}
+
+lotedom1355()
